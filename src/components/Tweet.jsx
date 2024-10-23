ff9313a02a7ebc089514e298720d23033cc9606b
Rahul Raj <raj.rahul686@gmail.com>
2024-10-23 23:27:29 +0530

    added the edit and save button funcationality

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import "../Css/Tweet.css";
 const Tweet = ({ tweetContent, likedCount, createdAt }) => {
+  const [isEditting, setIsEditting] = useState(false);
+  const handleEditTweet = () => {
+    setIsEditting((prevState) => !prevState);
+  };
   return (
     <div className="tweet-wrapper">
-      <div className="tweet-content">{tweetContent}</div>
+      <div className="tweetContent_wrapper">
+        <div className="tweet-content">
+          {(isEditting && <input />) || tweetContent}
+        </div>
+        <div className="editBtn">
+          <button onClick={handleEditTweet}>
+            {(isEditting && "Edit") || "Save"}
+          </button>
+        </div>
+      </div>
       <div className="created_liked_tweet_Wrapper">
         <div className="tweet-liked"> {likedCount} likes</div>
         <div className="createdTime">
